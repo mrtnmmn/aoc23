889852148ac389d4f6fa19ca038d8c69bc085268
Martin Lomba Rodriguez <mrtn.lmba@gmail.com>
2023-12-09 20:13:45 +0100

    modified CommonUtils

--- src/main/java/org/example/commons/CommonUtils.java
+++ src/main/java/org/example/commons/CommonUtils.java
@@ -6,11 +6,13 @@ import java.util.List;
 public class CommonUtils {
 
     public void logList (final List<String> list) {
+        System.out.println("Starting Log:");
         int iterations = 0;
         for (String value: list) {
-            System.out.println("L" + iterations + ": " + value);
+            System.out.println("Item " + iterations + ": " + value);
             iterations += 1;
         }
+        System.out.println(" ");
     }
 
     public List<char[]> stringListToCharList(List<String> fileLines) {
